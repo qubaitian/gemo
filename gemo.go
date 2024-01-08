41e3396fd0fffed4be17ea81382b25c548a6b63b
qubaitian <2501055750@qq.com>
2024-01-08 17:45:28 +0800

    init

---
+++ gemo.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	// Get a greeting message and print it.
+	fmt.Println("hello,world")
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/qubaitian/gemo
+
+go 1.21.5
