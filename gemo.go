4c1b46774042576879dd9da25fde330c24293ac0
qubaitian <2501055750@qq.com>
2024-01-08 17:47:34 +0800

    init

--- gemo.go
+++ gemo.go
@@ -4,7 +4,9 @@ import (
 	"fmt"
 )
 
-func main() {
-	// Get a greeting message and print it.
-	fmt.Println("hello,world")
+// Hello returns a greeting for the named person.
+func Hello(name string) string {
+	// Return a greeting that embeds the name in a message.
+	message := fmt.Sprintf("Hi, %v. Welcome!", name)
+	return message
 }
