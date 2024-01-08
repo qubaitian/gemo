ee9fe77fe200dae6cdb121809107752a0773c5cd
qubaitian <2501055750@qq.com>
2024-01-08 18:28:37 +0800

    init

--- gemo.go
+++ gemo.go
@@ -1,12 +1,18 @@
-package gemo
+package greetings
 
 import (
-	"fmt"
+	"errors"
 )
 
 // Hello returns a greeting for the named person.
-func Hello(name string) string {
-	// Return a greeting that embeds the name in a message.
-	message := fmt.Sprintf("Hi, %v. Welcome!", name)
-	return message
+func Hello(name string) (string, error) {
+	// If no name was given, return an error with a message.
+	if name == "" {
+		return "", errors.New("empty name")
+	}
+
+	// If a name was received, return a value that embeds the name
+	// in a greeting message.
+	message := ""
+	return message, nil
 }
