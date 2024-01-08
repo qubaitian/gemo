c8d926d9a6b81068d8b03f42aa2047f109acac07
qubaitian <2501055750@qq.com>
2024-01-08 18:35:28 +0800

    all

---
+++ gemo_test.go
@@ -0,0 +1,14 @@
+package gemo
+
+import (
+	"testing"
+)
+
+// TestHelloEmpty calls greetings.Hello with an empty string,
+// checking for an error.
+func TestHelloEmpty(t *testing.T) {
+	msg, err := Hello("")
+	if msg != "" || err == nil {
+		t.Fatalf(`Hello("") = %q, %v, want "", error`, msg, err)
+	}
+}
